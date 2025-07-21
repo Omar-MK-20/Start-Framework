cd85ecece96b033853d02a824dba12bf32e18419
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-07-21 22:40:13 +0300

    Enhance navbar styling with padding and rounded corners for nav links

--- src/Layout/NavBar/NavBar.jsx
+++ src/Layout/NavBar/NavBar.jsx
@@ -25,20 +25,20 @@ function NavBar() {
         <>
             <div className="navbar navbar-expand-lg bg-dark-blue fixed-top">
                 <div className={navLen ? "container" : "container py-3"}>
-                    <NavLink className="navbar-brand text-white fs-2 fw-bold" to="/">START FRAMEWORK</NavLink>
+                    <NavLink className="navbar-brand text-white fs-2 fw-bold px-3" to="/">START FRAMEWORK</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarItems" aria-controls="navbarItems" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarItems">
                         <ul className="navbar-nav ms-auto column-gap-3 px-2 mb-2">
                             <li className="nav-item">
-                                <NavLink className="nav-link text-white fw-bold" aria-current="page" to='about'>ABOUT</NavLink>
+                                <NavLink className="nav-link text-white fw-bold px-2 rounded-2" aria-current="page" to='about'>ABOUT</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link text-white fw-bold" aria-current="page" to='portfolio'>PORTFOLIO</NavLink>
+                                <NavLink className="nav-link text-white fw-bold px-2 rounded-2" aria-current="page" to='portfolio'>PORTFOLIO</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link text-white fw-bold" aria-current="page" to='contact'>CONTACT</NavLink>
+                                <NavLink className="nav-link text-white fw-bold px-2 rounded-2" aria-current="page" to='contact'>CONTACT</NavLink>
                             </li>
                         </ul>
                     </div>
